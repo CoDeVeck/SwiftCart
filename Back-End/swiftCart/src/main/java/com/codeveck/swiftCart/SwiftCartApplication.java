f3690c79fd72b7882a83aa57c6bd7d50c3544424
Vector404 <victornarazas2005@gmail.com>
2026-03-22 16:58:45 -0500

    🎉  start(swiftcart) : inicializando proyecto

---
+++ Back-End/swiftCart/src/main/java/com/codeveck/swiftCart/SwiftCartApplication.java
@@ -0,0 +1,13 @@
+package com.codeveck.swiftCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftCartApplication.class, args);
+	}
+
+}
